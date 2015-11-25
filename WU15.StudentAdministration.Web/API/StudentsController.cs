19708c6e2706f1e3aeee19f38cfa5d7b4045ed8d
Trexxi <tr3xxy@hotmail.com>
2015-11-25 13:51:38 +0100

    Changed a lot of things

--- WU15.StudentAdministration.Web/API/StudentsController.cs
+++ WU15.StudentAdministration.Web/API/StudentsController.cs
@@ -24,12 +24,24 @@ namespace WU15.StudentAdministration.Web.API
         {
             if (student.Id == 0)
             {
-                var id = MvcApplication.Students.Max(x => x.Id) + 1;
-                student.Id = id;
+                if (MvcApplication.Students.Any())
+                {
+                    var id = MvcApplication.Students.Max(x => x.Id) + 1;
+                    student.Id = id;
+                }
+                else
+                {
+                    student.Id = 1;
+                }
+            }
+            else
+            {
+                var savedIndex = MvcApplication.Students.FindIndex(x => x.Id == student.Id);
+                MvcApplication.Students.RemoveAt(savedIndex);
             }
             MvcApplication.Students.Add(student);
 
-            return string.Format("{0} {1}", student.FirstName, student.LastName);       
+            return string.Format("{0} {1}", student.FirstName, student.LastName);
         }
 
         [HttpDelete]
